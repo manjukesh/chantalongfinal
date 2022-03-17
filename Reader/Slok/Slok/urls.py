89abf7af149f75ad6c06af683363e6a5f507c3f1
Manjukeshwar Reddy <manjukesh.india.39@gmail.com>
2022-03-17 10:18:40 +0530

    Update urls.py

--- Reader/Slok/Slok/urls.py
+++ Reader/Slok/Slok/urls.py
@@ -2,7 +2,11 @@ from Slok import views
 from django.conf.urls import url
 from django.urls import path
 
+def okay(request):
+    return HttpResponse('pretend-binary-data-here', content_type='image/jpeg')
+
 urlpatterns = [
+    path('favicon.ico', okay),
     url(r'^$', views.index),
     url('mantra', views.mantra),
     path("clips/<str:filename>/", views.mantra_audio),
