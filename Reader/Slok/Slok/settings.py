5f0b77a1a2385f542cfa14a9535480b6644d6008
Manjukeshwar Reddy <manjukesh.india.39@gmail.com>
2022-03-17 09:41:59 +0530

    Update settings.py

--- Reader/Slok/Slok/settings.py
+++ Reader/Slok/Slok/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_ROOT = BASE_DIR
+
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
 # Quick-start development settings - unsuitable for production
@@ -135,4 +137,4 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'Slok/static/')
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, ‘static’)
+
