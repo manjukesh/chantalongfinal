3e83d2f2c4992d9e8c8943543a280faf65d71d2e
Manjukeshwar Reddy <manjukesh.india.39@gmail.com>
2022-03-18 06:43:20 +0530

    Update wsgi.py

--- Reader/Slok/Slok/wsgi.py
+++ Reader/Slok/Slok/wsgi.py
@@ -8,10 +8,12 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
+import dj_static
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Reader.Slok.Slok.settings')
 from django.core.wsgi import get_wsgi_application
 
-application = get_wsgi_application()
+from dj_static import Cling
+application = Cling(get_wsgi_application())
